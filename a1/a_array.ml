91aec569044d4ed6c128deafb56122684678769f
recurze <vrahul24999@gmail.com>
2018-05-02 18:34:55 +0530

    Made it easy to read

--- a1/a_array.ml
+++ a1/a_array.ml
@@ -5,12 +5,15 @@ exception TooShort of string;;
 
 type letter = char;;
 (* type letter = alphabet;; *)
-type astar = {marker:int ref; strlist: letter array; length:int ref };;
+type astar = {marker:int ref;
+              strlist: letter array;
+              length:int ref }
+;;
 
 (*
-    The comments after the function define the signature of that function. 
-    And additional comments required by the assignment 
-    for time complexity and proofs.
+    The comments after the function define the signature
+    of that function. And additional comments required
+    by the assignment for time complexity and proofs.
 *)
 
 let lgh s = !(s.length);;
@@ -19,16 +22,18 @@ let lgh s = !(s.length);;
 let nonempty s = (!(s.length)!=0) ;;
 (* val nonempty : astar -> bool = <fun>  *)
 
-let concat a b = {marker=ref 0; 
+let concat a b = {marker=ref 0;
                   strlist=Array.append a.strlist b.strlist;
                   length=ref (!(a.length)+ (!(b.length)))
-                 };;
+                 }
+;;
 (* val concat : astar -> astar -> astar = <fun>  *)
 (*
-    Time complexity of (concat a b) = O(n+m) where n, m are the lengths
-    of a, b resp.
+    Time complexity of (concat a b) = O(n+m)
+    where n, m are the lengths of a, b resp.
     lgh (concat a b) = (lgh a) + (lgh b)
-    I defined len as n+m, as extending a by b results in a list of n+m elements.
+    I defined len as n+m, as extending a by b results
+    in a list of n+m elements
  *)
 
 let reverse s =
@@ -37,26 +42,33 @@ let reverse s =
     for i=0 to n-1 do
         a.(i) <- (s.strlist).(n-1-i) done;
 
-    {marker=ref 0; strlist=a; length=s.length};;
+    {marker=ref 0; strlist=a; length=s.length}
+;;
 (* val reverse : astar -> astar = <fun> *)
 (*
     len (reverse s) = len s
-    Once again, len of reverse s is defined as len of s, as no elements are
-    added or removed from the charlist.
+    Once again, len of reverse s is defined as len of s,
+    as no elements are added or removed from the charlist.
  *)
 
 let first s =
-    if (!(s.length)!=0) then s.strlist.(0)
-    else raise (Empty "The string is empty");;
+    if (!(s.length)!=0) then
+        s.strlist.(0)
+    else
+        raise (Empty "The string is empty")
+;;
 (* val first : astar -> letter = <fun>  *)
 
 let last s =
-    if (!(s.length)!=0) then s.strlist.(!(s.length)-1)
-    else raise (Empty "The string is empty");;
+    if (!(s.length)!=0) then
+        s.strlist.(!(s.length)-1)
+    else
+        raise (Empty "The string is empty")
+;;
 (* val last : astar -> letter = <fun> *)
 
 let create s =
-    if s="" then 
+    if s="" then
         {marker=ref 0; strlist = [||]; length=ref 0}
     else begin
         let n = String.length s in
@@ -69,34 +81,47 @@ let create s =
 (* val create : string -> astar = <fun>  *)
 
 let forward s =
-    if !(s.marker) != !(s.length)-1 then s.marker:=!(s.marker)+1
-    else raise (AtLast "Reached the end of the string");;
+    if !(s.marker) != !(s.length)-1 then
+        s.marker:=!(s.marker)+1
+    else
+        raise (AtLast "Reached the end of the string")
+;;
 (* val forward : astar -> unit = <fun> *)
-(* 
-    Time complexity of this function is = O(1), finite expressions to be evaled.
+(*
+    Time complexity of this function is = O(1),
+    finite expressions to be evaled.
 *)
 
 let back s =
-    if !(s.marker) != 0 then s.marker:=!(s.marker)-1
-    else raise (AtFirst "Reached the start of the string");;
+    if !(s.marker) != 0 then
+        s.marker:=!(s.marker)-1
+    else
+        raise (AtFirst "Reached the start of the string")
+;;
 (* val back : astar -> unit = <fun> *)
-(* Time complexity of this function is O(1), reason mentioned above *)
+(* Time complexity of this function is O(1),
+ * reason mentioned above
+ *)
 
 let moveTo n s =
-    if n < !(s.length) then s.marker:=n
-    else raise (TooShort "The string doesn't contain so many characters");;
+    if n < !(s.length) then
+        s.marker:=n
+    else
+        raise (TooShort "The string doesn't contain so many characters")
+;;
 (* val moveTo : int -> astar -> unit = <fun> *)
 (*
-    Time complexity of this function is O(1) better than required O(n).
-    O(n) algorithm for this function is to call forward at most n times.
-    (or backward depending on the current position of marker) 
+    Time complexity of this function is O(1) better
+    than required O(n). O(n) algorithm for this function
+    is to call forward at most n times.
+    (or backward depending on the current position of marker)
 *)
 
 let replace w s = s.strlist.(!(s.marker)) <- w;;
 (* val replace : letter -> astar -> astar = <fun> *) 
 (*
     len (replace a b) = len b
-    One element of the list is being replaced by another so effectively,
-    no elements have been added or removed. So the number of elements and so the
-    length remains same.
+    One element of the list is being replaced by another
+    so effectively, no elements have been added or removed.
+    So the number of elements and so the length remains same.
 *)
--- a1/a_list.ml
+++ a1/a_list.ml
@@ -5,12 +5,15 @@ exception TooShort of string;;
 
 type letter = char;;
 (* type letter = alphabet;; *)
-type astar = {marker:int ref; mutable strlist: letter list; length:int ref };;
+type astar = {marker:int ref;
+              mutable strlist: letter list;
+              length:int ref }
+;;
 
 (*
-    The comments after the function define the signature of that function.
-    And additional comments required by the assignment
-    for time complexity and proofs.
+    The comments after the function define the signature
+    of that function. And additional comments required
+    by the assignment for time complexity and proofs.
 *)
 
 let lgh s = !(s.length);;
@@ -19,84 +22,118 @@ let lgh s = !(s.length);;
 let nonempty s = (!(s.length)!=0) ;;
 (* val nonempty : astar -> bool = <fun>  *)
 
-let concat a b = {marker=ref 0; 
+let concat a b = {marker=ref 0;
                   strlist=a.strlist @ b.strlist;
                   length=ref (!(a.length)+ (!(b.length)))
-                 };;
+                 }
+;;
 (* val concat : astar -> astar -> astar = <fun>  *)
-(* 
-    Time complexity of (concat a b) = O(min(n, m)) where n, m are the lengths
-    of a, b resp.
+(*
+    Time complexity of (concat a b) = O(min(n, m))
+    where n, m are the lengths of a, b resp.
     lgh (concat a b) = (lgh a) + (lgh b)
-    I defined len as n+m, as extending a by b results in a list of n+m elements
+    I defined len as n+m, as extending a by b results
+    in a list of n+m elements
  *)
 
 let reverse s =
-    let rec rev a = match a with 
-                    [] -> [] |
-                    x::y -> (rev y) @ [x] in
-    {marker=ref 0; strlist=(rev s.strlist); length=s.length};;
+    let rec rev a =
+        match a with
+            [] -> []
+        |   x::y -> (rev y) @ [x] in
+    {marker=ref 0;
+     strlist=(rev s.strlist);
+     length=s.length}
+;;
 (* val reverse : astar -> astar = <fun> *)
 (*
     len (reverse s) = len s
-    Once again, len of reverse s is defined as len of s, as no elements are
-    added or removed from the charlist.
+    Once again, len of reverse s is defined as len of s,
+    as no elements are added or removed from the charlist.
  *)
 
 let first s =
-    if (!(s.length)!=0) then List.nth s.strlist 0
-    else raise (Empty "The string is empty");;
+    if (!(s.length)!=0) then
+        List.nth s.strlist 0
+    else
+        raise (Empty "The string is empty")
+;;
 (* val first : astar -> letter = <fun>  *)
 
-let last s = 
-    if (!(s.length)!=0) then List.nth s.strlist (!(s.length)-1)
-    else raise (Empty "The string is empty");;
+let last s =
+    if (!(s.length)!=0) then
+        List.nth s.strlist (!(s.length)-1)
+    else
+        raise (Empty "The string is empty")
+;;
 (* val last : astar -> letter = <fun> *)
 
 let create s =
-    let rec conv i a = if i<0 then a else (conv (i-1) (s.[i] :: a)) in
-    let rec len i a = match a with
-                        []-> ref i |
-                        h::t-> len (i+1) t in
+    let rec conv i a =
+        if i<0 then a
+        else (conv (i-1) (s.[i] :: a))
+    in
+    let rec len i a =
+        match a with
+            []-> ref i
+        |   h::t-> len (i+1) t
+    in
     let temp = conv ((String.length s)-1) [] in
-    {marker=ref 0; strlist=temp; length=(len 0 temp)};;
+    {marker=ref 0; strlist=temp; length=(len 0 temp)}
+;;
 (* val create : string -> astar = <fun>  *)
 
 let forward s =
-    if !(s.marker) != !(s.length)-1 then s.marker:=!(s.marker)+1
-    else raise (AtLast "Reached the end of the string");;
+    if !(s.marker) != !(s.length)-1 then
+        s.marker:=!(s.marker)+1
+    else
+        raise (AtLast "Reached the end of the string")
+;;
 (* val forward : astar -> unit = <fun> *)
 (*
-    Time complexity of this function is = O(1), constant number of expressions to be evaled.
+    Time complexity of this function is = O(1),
+    constant number of expressions to be evaled.
 *)
 
 let back s =
-    if !(s.marker) != 0 then s.marker:=!(s.marker)-1
-    else raise (AtFirst "Reached the start of the string");;
+    if !(s.marker) != 0 then
+        s.marker:=!(s.marker)-1
+    else
+        raise (AtFirst "Reached the start of the string")
+;;
 (* val back : astar -> unit = <fun> *)
-(* Time complexity of this function is O(1), reason mentioned above *)
+(* Time complexity of this function is O(1),
+ * reason mentioned above
+ *)
 
 let moveTo n s =
-    if n < !(s.length) then s.marker:=n
-    else raise (TooShort "The string doesn't contain so many characters");;
+    if n < !(s.length) then
+        s.marker:=n
+    else
+        raise (TooShort "The string doesn't contain so many characters")
+;;
 (* val moveTo : int -> astar -> unit = <fun> *)
 (*
-    Time complexity of this function is O(1) better than required O(n).
-    O(n) algorithm for this function is to call forward at most n times.
+    Time complexity of this function is O(1) better
+    than required O(n). O(n) algorithm for this function
+    is to call forward at most n times.
     (or backward depending on the current position of marker)
 *)
 
-let replace w s = 
-    let rec rep i c a = match a with
-                        []->[] |
-                        x::y->
-                            if i=0 then c::y
-                            else x::(rep (i-1) c y) in
-    s.strlist<-(rep !(s.marker) w s.strlist);;
+let replace w s =
+    let rec rep i c a =
+        match a with
+            []->[]
+        |   x::y->
+                if i=0 then c::y
+                else x::(rep (i-1) c y)
+    in
+    s.strlist<-(rep !(s.marker) w s.strlist)
+;;
 (* val replace : letter -> astar -> astar = <fun> *) 
 (*
     len (replace a b) = len b
-    One element of the list is being replaced by another so effectively,
-    no elements have been added or removed. So the number of elements and so the
-    length remains same.
+    One element of the list is being replaced by another
+    so effectively, no elements have been added or removed.
+    So the number of elements and so the length remains same.
 *)
